c809870665f84fbfeff16ffc71a770c846d4eef0
Matt Casperson <mc.casperson@gmail.com>
2021-12-07 10:35:04 -0800

    add second and third specs and code to pass
    Co-Authored-by: Marisa Edgar <marisaedgar1212@gmail.com>
    Co-authored-by: Frank Proulx <frankproulx7@gmail.com>

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,9 +1,22 @@
 class WordMaker
   def initialize
     @ones = { 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine' }
+    @teens = { 0 => 'ten', 1 => 'eleven', 2 => 'twelve', 3 => 'thirteen', 4 => 'fourteen', 5 => 'fifteen', 6 => 'sixteen', 7 => 'seventeen', 8 => 'eighteen', 9 => 'nineteen' }
+    @tens = { 2 => 'twenty', 3 => 'thirty', 4 => 'fourty', 5 => 'fifty', 6 => 'sixty', 7 => 'seventy', 8 => 'eighty', 9 => 'ninety' }
   end
 
   def wordizer(number)
-    @ones.fetch(number)
+    array = number.to_s.split("")
+    number_array = []
+    array.each do |number|
+      number_array.push(number.to_i)
+    end
+    if number_array.length == 1
+      @ones.fetch(number)
+    elsif (number_array.length == 2) && (number_array[0] == 1)
+      @teens.fetch(number_array[1])
+    else 
+      @tens.fetch(number_array[0]) + ' ' + @ones.fetch(number_array[1])
+    end
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -6,4 +6,15 @@ describe('WordMaker#wordizer') do
     word1 = WordMaker.new()
     expect(word1.wordizer(7)).to(eq('seven'))
   end
+
+  it('takes a two digit number between ten and nineteen and returns the written word version') do
+    word1 = WordMaker.new()
+    expect(word1.wordizer(12)).to(eq('twelve'))
+  end
+
+  it('takes any two digit number and returns the written word version') do
+    word1 = WordMaker.new()
+    expect(word1.wordizer(69)).to(eq('sixty nine'))
+  end
+
 end
